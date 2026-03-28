f69fd63db1fe75a9bb1bfc799cfce4857e6d9fad
waleedju88 <waleedunt@gmail.com>
2026-03-28 18:20:33 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE = "hydrotrack-v1";
+const ASSETS = ["/hydrotrack/", "/hydrotrack/index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+// Handle notification clicks
+self.addEventListener("notificationclick", e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow("/hydrotrack/"));
+});
